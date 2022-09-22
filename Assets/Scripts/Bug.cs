4e74edf0334f1a7618169091457668b9c4b179fa
Connor Chang <connorc0627@gmail.com>
2022-09-22 15:57:52 -0700

    Make bees move randomly in rooms

--- Assets/Scripts/Bug.cs
+++ Assets/Scripts/Bug.cs
@@ -38,10 +38,49 @@ public class Bug : MonoBehaviour
         transform.position = _workRoom.transform.position + (new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f)));
     }
 
+    float moveTimer = 0;
+    bool moving = false;
+    void Update()
+    {
+        if (moving) return;
+
+        moveTimer += Time.deltaTime;
+        if (moveTimer > 1)
+        {
+            moveTimer -= 1;
+            if (Random.Range(0, 3) == 0)
+            {
+                StartCoroutine(MoveTo(transform.position, RandomTarget()));
+            }
+        }
+    }
+
+    Vector2 RandomTarget()
+    {
+        Vector2 pos;
+        do
+        {
+            pos = new Vector2(Random.Range(-10f, 10f), Random.Range(-10f,10f));
+        } while ( !_workRoom.GetComponent<Collider2D>().OverlapPoint(pos) );
+        return pos;
+    }
+
+    IEnumerator MoveTo(Vector2 start, Vector2 end)
+    {
+        moving = true;
+        const int total = 30;
+        for (int i = 0; i < total; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+            transform.position = Vector2.Lerp(start, end, ((float)i)/total);
+        }
+        moving = false;
+    }
+
     IEnumerator DelayMove() {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.1f);
         WorkRoom.AddBug(this);
-        transform.position = WorkRoom.transform.position + (new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f)));
+        transform.position = RandomTarget();
     }
 
 }
